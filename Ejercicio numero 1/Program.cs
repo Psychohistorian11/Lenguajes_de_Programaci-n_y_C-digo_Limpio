8a000c49fe3e1cbfa148fe23abecd552993fe250
Cristian <cristianfrancoraigosa@gmail.com>
2023-02-05 17:36:50 -0500

    Ejercicio numero 1: Resuelto Ejercicio numero 2: En proceso

--- Ejercicio numero 1/Program.cs
+++ Ejercicio numero 1/Program.cs
@@ -1,2 +1,70 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Ejercicio_numero_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Teorema de la Desigualdad Triangular:
+            la suma de las longitudes de dos lados cualesquiera
+            de un triángulo es mayor o igual que la longitud del tercer lado.*/
+
+            //Algoritmo de Compruebe la Desigualdad Triangular
+            Console.WriteLine("la suma de las longitudes de dos lados cualesquiera " +
+                "de un triángulo es mayor o igual que la longitud del tercer lado");
+
+            Console.WriteLine("Digite ladoA: ");
+            Double ladoA = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Digite ladoB: ");
+            Double ladoB = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Digite ladoC: ");
+            Double ladoC = Convert.ToDouble(Console.ReadLine());
+
+            Verificar_Desigualdad_Triangular(ladoA, ladoB, ladoC);
+        }
+
+        static void Verificar_Desigualdad_Triangular(Double ladoA, Double ladoB, Double ladoC)
+        {
+            if((ladoA + ladoB) > ladoC) 
+            {
+               if((ladoA + ladoC) > ladoB)
+                {
+                    if((ladoC + ladoB) > ladoA)
+                    {
+                        Cumple_Desigualdad_Triangular(ladoA, ladoB, ladoC);
+                    }
+                    else
+                    {
+                        No_cumple_Desigualdad_Triangular(ladoC, ladoB, ladoA);
+                    }
+                }
+                else
+                {
+                    No_cumple_Desigualdad_Triangular(ladoA, ladoC, ladoB);
+                }
+            }
+            else
+            {
+                No_cumple_Desigualdad_Triangular(ladoA, ladoB, ladoC);
+            }
+        }
+
+        static void Cumple_Desigualdad_Triangular(Double ladoA, Double ladoB, Double ladoC)
+        {
+            Double ladoAB = (ladoA + ladoB);
+            Double ladoBC = (ladoB + ladoC);
+            Double ladoAC = (ladoA + ladoC);
+            Console.WriteLine("Cumple Teorema de Desigualdad Triangular porque: ");
+            Console.WriteLine("El ladoA: " + ladoA + " + El ladoB: " + ladoB + " = " + ladoAB + " > ladoC: " + ladoC);
+            Console.WriteLine("El ladoA: " + ladoA + " + El ladoC: " + ladoC + " = " + ladoAC + " > ladoB: " + ladoB);
+            Console.WriteLine("El ladoB: " + ladoB + " + El ladoC: " + ladoC + " = " + ladoBC + " > ladoA: " + ladoA);
+        }
+
+        static void No_cumple_Desigualdad_Triangular(Double ladoX, Double ladoY, Double ladoZ)
+        {
+            Console.WriteLine("No cumple Teorema de Desigualdad Triangular porque: ");
+            Console.WriteLine("El lado: "+ ladoX + " + El lado: " + ladoY + " < que el lado: "+ ladoZ);
+        }
+    }
+}
--- Ejercicio numero_2/Program.cs
+++ Ejercicio numero_2/Program.cs
@@ -1,2 +1,60 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Ejercicio_numero_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*  En un curso se practican 4
+                evaluaciones con los siguientes
+                porcentajes: 25%, 20%, 25% y 30%,
+                respectivamente. Por cada estudiante
+                se informa código y las cuatro notas.
+                Hacer un algoritmo que calcule la
+                nota definitiva de cada estudiante, el
+                promedio de notas definitivas del
+                curso y el porcentaje de perdedores.*/
+
+            List<int> studentsList = new List<int>();
+            List<int> student = new List<int>();
+
+            Console.WriteLine("Ingrese la cantidad de Estudiantes en el curso: ");
+            int NumberOfStudents = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < NumberOfStudents; i++)
+            {
+
+                for (int j = 0; j < 1; j++)
+                {   //Codigo del estudiante
+                    Console.WriteLine("Digite el Codigo del estudiante: ");
+                    student.Add(Convert.ToInt32(Console.ReadLine()));
+                    //Nota del 25%
+                    Console.WriteLine("Digite Nota del 25%: ");
+                    student.Add(Convert.ToInt32(Console.ReadLine()));
+                    //Nota del 20%
+                    Console.WriteLine("Digite Nota del 20%: ");
+                    student.Add(Convert.ToInt32(Console.ReadLine()));
+                    //Nota del 25%
+                    Console.WriteLine("Digite Nota del 25%: ");
+                    student.Add(Convert.ToInt32(Console.ReadLine()));
+                    //Nota del 30%
+                    Console.WriteLine("Digite Nota del 30%: ");
+                    student.Add(Convert.ToInt32(Console.ReadLine()));
+
+                }
+                studentsList.AddRange(student);
+
+            }
+
+            foreach(var i in studentsList)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.WriteLine(studentsList);
+            Console.WriteLine("hloa");
+            Console.WriteLine(studentsList.Count);
+        }
+    }
+}
